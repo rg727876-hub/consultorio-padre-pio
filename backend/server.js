86ae8487048823eacf6a2581d9a822be482b6991
rg727876-hub <rg727876@gmail.com>
2026-05-21 00:38:55 -0500

    chore: trigger redeploy en Render

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { startCronJobs } = require('./src/jobs/expireAppointments');
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`[v2] Servidor corriendo en http://localhost:${PORT}`);
     console.log(`Entorno: ${process.env.NODE_ENV}`);
 
   // Iniciar jobs programados
